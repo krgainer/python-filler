dc4b1907885fcc25b216c2ca9912cde140e77778
Kieran <86863603+krgainer@users.noreply.github.com>
2025-06-14 17:28:45 -0400

    finding my footing

--- filler.py
+++ filler.py
@@ -1,6 +1,7 @@
 import random
+import time
 
-def generate_inital_board(gameboard_array):
+def generate_inital_board(gameboard_array, square, color_set):
 	#generate gameboard
 	for i in range(square):
 		temp_list=[]
@@ -11,20 +12,25 @@ def generate_inital_board(gameboard_array):
 		gameboard_array.append(temp_list)
 		del temp_list
 	#genrerate player colors
-	player_one_currenet_color = gameboard[0][-1]
-	player_two_currenet_color = gameboard[-1][0]
-	while player_one_currenet_color == player_two_currenet_color:
+	while gameboard_array[0][-1] == gameboard_array[-1][0]:
 		print("duplicate, swapping @ ",player_one_currenet_color,player_two_currenet_color)
-		gameboard[-1][0] = random.randrange(0, color_set)
-		player_two_currenet_color = gameboard[-1][0]
+		gameboard_array[-1][0] = random.randrange(0, color_set)
+		player_two_currenet_color = gameboard_array[-1][0]
 	# return board and player colors
-	return gameboard_array, [player_one_currenet_color, player_two_currenet_color]
+	return gameboard_array
 
 def helpful_print(value:list):
 	for i in value:
 		print(i)
 
-def available_colors(curr_color, prev_color):
+def player_current_color(player,gameboard):
+	if player == 0:
+		return gameboard[0][-1]
+	else:
+		return gameboard[-1][0]
+
+def available_colors(gameboard, prev_color,color_set):
+	curr_color = [player_current_color(0,gameboard),player_current_color(1,gameboard)]
 	color_list = range(color_set)
 	available_color_set = []
 	for elm in color_list:
@@ -32,8 +38,10 @@ def available_colors(curr_color, prev_color):
 			available_color_set.append(elm)
 	return available_color_set
 
-def swap_colors(player,target_color):
-	pass
+def swap_colors(player, player_previous_colors, player_current_colors, target_color):
+	player_previous_colors[player] = player_current_colors[player]
+	player_current_colors[player] = target_color
+	return player_previous_colors, player_current_colors
 
 def check_win_condition():
 	pass
@@ -41,8 +49,22 @@ def check_win_condition():
 def game():
 	pass
 
+def traverse(player, player_current_colors, color_set, gameboard):
+	# this may or may not be the acutally recursive traversal function. 
+	# it must spread the new color across current territory, capture adjacent cells, and count teritory. 
+	# I'm tempted to create two "mask" arrays for each player, but that would get memory intesive kinda quickly? I mean each val in the array can be bool, but theres two of them.
+	if player == 0:
+		flood_fill(0,-1,color_set+1,gameboard,player)
+	else:
+		flood_fill(-1,0,color_set+1,gameboard,player)
+
+
+def flood_fill(row, col, new_color, gameboard, player):
+	pass
+
+
+
 def main():
-	global gameboard, color_set, square
 	color_set = 7
 	square = 6
 	gameboard = []
@@ -50,14 +72,15 @@ def main():
 	# this set is player 1, then player 2
 	player_scores = [1,1]
 	player_previous_colors = [None, None]
-	player_current_colors = [None, None]
-	gameboard, player_current_colors = generate_inital_board(gameboard)
-	print(f"Player One Number: {player_current_colors[0]} \nPlayer Two Number: {player_current_colors[1]}")
+	gameboard = generate_inital_board(gameboard,square, color_set)
+	print(f"Player One Number: {player_current_color(0,gameboard)} \nPlayer Two Number: {player_current_color(1,gameboard)}")
 	while (check_win_condition()) == None:
 		print("-"*200)
+		print(f"Player One Current Score: {player_scores[0]}\nPlayer Two Current Score: {player_scores[1]}")
 		helpful_print(gameboard)
-		if current_player == 1:
-			print(f"Player 1 - You can chose from any of these numbers: {available_colors(player_current_colors,player_previous_colors)}")
+		if current_player == 0:
+			print(f"Player 1 - You can chose from any of these numbers: {available_colors(gameboard,player_previous_colors,color_set)}")
+			time.sleep(10)
 			
 
 main()
---
+++ notes.md
@@ -0,0 +1,7 @@
+In my head this is less fucked than it is.
+
+I'm guessing I should do a recursive travel thing, passing current square. It checks two things, if current sqaure is current players current color, and if its a edge (ignoring the last traversed square.)
+
+This will not be fun
+
+
